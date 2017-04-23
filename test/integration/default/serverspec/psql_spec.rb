4005562141cb57c9f9d2aba03696da14661fe7f9
juju4 <juju4@users.noreply.github.com>
2017-04-23 17:14:59 -0400

    separate db.yml with become _zentral for peer authentication, serverspec test

---
+++ test/integration/default/serverspec/psql_spec.rb
@@ -0,0 +1,20 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+## Use Junit formatter output, supported by jenkins
+#require 'yarjuf'
+#RSpec.configure do |c|
+#    c.formatter = 'JUnit'
+#end
+
+describe command("psql -U _zentral zentraldb -c '\dt'") do
+  its(:stdout) { should match /accounts_user/ }
+  its(:stdout) { should match /inventory_machinetag/ }
+  its(:stdout) { should match /probes_feed/ }
+  its(:stdout) { should_not match /No relations found./ }
+  its(:exit_status) { should eq 0 }
+  let(:sudo_options) { '-u _zentral -H' }
+end
+
