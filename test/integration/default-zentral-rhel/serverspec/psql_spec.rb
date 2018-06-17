f0a3c00b155f474e1754927a0399822be99ac68f
juju4 <juju4@users.noreply.github.com>
2018-06-16 23:03:41 -0400

    serverspec/rhel: sudo_options not passed, included in command

--- test/integration/default-zentral-rhel/serverspec/psql_spec.rb
+++ test/integration/default-zentral-rhel/serverspec/psql_spec.rb
@@ -9,12 +9,12 @@ set :backend, :exec
 #    c.formatter = 'JUnit'
 #end
 
-describe command("psql -U _zentral zentraldb -c '\\dt'") do
+describe command("sudo -u _zentral -H psql -U _zentral zentraldb -c '\\dt'") do
   its(:stdout) { should match /accounts_user/ }
   its(:stdout) { should match /inventory_machinetag/ }
   its(:stdout) { should match /probes_feed/ }
   its(:stdout) { should_not match /No relations found./ }
   its(:exit_status) { should eq 0 }
-  let(:sudo_options) { '-u _zentral -H' }
+#  let(:sudo_options) { '-u _zentral -H' }
 end
 
