0833951a52654fb77fe74f42140555d93a59e79e
juju4 <juju4@users.noreply.github.com>
2019-05-12 09:36:09 -0400

    test/default: add rabbitmq serverspec tests

---
+++ test/integration/default/serverspec/rabbitmq_spec.rb
@@ -0,0 +1,52 @@
+#require 'spec_helper'
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe package('rabbitmq-server'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('rabbitmq-server'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('rabbitmq-server'), :if => os[:family] == 'redhat' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('rabbitmq-server'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(5671) do
+  it { should be_listening }
+end
+
+describe command('rabbitmqctl status') do
+  its(:stdout) { should match /Status of node / }
+  its(:stdout) { should match /{memory,/ }
+  its(:stdout) { should match /{kernel,{net_ticktime/ }
+  its(:stdout) { should match /{pid,/ }
+  its(:stdout) { should match /{asn1,/ }
+  its(:stdout) { should match /{rabbit_common,/ }
+  its(:stdout) { should match /\[{rabbit,"RabbitMQ"/ }
+  its(:exit_status) { should eq 0 }
+  let(:sudo_options) { '-u rabbitmq -H' }
+end
+
+describe command('rabbitmqctl authenticate_user guest guest') do
+  its(:stdout) { should_not match /Error: failed to authenticate user/ }
+  its(:exit_status) { should eq 0 }
+  let(:sudo_options) { '-u rabbitmq -H' }
+end
+
+describe command('rabbitmqctl eval \'ssl:versions().\'') do
+  its(:stdout) { should match /tlsv1.2/ }
+  its(:exit_status) { should eq 0 }
+  let(:sudo_options) { '-u rabbitmq -H' }
+end
+
